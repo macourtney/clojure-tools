12d3047d0e8aead9da05230a5a8f2e7966d407fd
Matt Courtney <macourtney@gmail.com>
2010-09-30 09:30:44 -0400

    Updated version to 1.0.0-RC1

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject clojure-tools "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject clojure-tools "1.0.0-RC1"
+  :description "Clojure tools is a set of clojure utilities pulled out of Conjure."
   :dependencies [[commons-lang/commons-lang "2.5"]
                  [org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]])
