4b995f7d87a2a6d949e93e26d2b4b6357b9691f5
Matthew Courtney <mcourtne199@yahoo.com>
2010-10-25 12:23:06 -0400

    Updated the version number to 1.0.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject clojure-tools "1.0.0-RC1"
+(defproject clojure-tools "1.0.0"
   :description "Clojure tools is a set of clojure utilities pulled out of Conjure."
   :dependencies [[commons-lang/commons-lang "2.5"]
                  [org.clojure/clojure "1.2.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]])
+                 [org.clojure/clojure-contrib "1.2.0"]]
+  :dev-dependencies [[lein-clojars "0.6.0"]])
