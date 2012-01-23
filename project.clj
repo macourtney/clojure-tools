ec132f0c20ae01a00f5825dcd790c43020c06910
Matt Courtney <macourtney@gmail.com>
2012-01-23 15:39:45 -0500

    Updated the version to 1.1.2-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-tools "1.1.1"
+(defproject clojure-tools "1.1.2-SNAPSHOT"
   :description "Clojure tools is a set of clojure utilities pulled out of Conjure."
   :dependencies [[commons-lang/commons-lang "2.5"]
                  [org.clojure/clojure "1.2.1"]
